24110c99a22e9a45ebacc2174e790d3825b84eec
KPreetha <47448806+KPreetha@users.noreply.github.com>
2020-07-09 19:37:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def home():
 
 @app.route("/get")
 def get_bot_response():
-    user_request = request.args.get('msg')
+    user_request = request.args.get('msg')  # Fetching input from the user
     user_request = user_request.lower()
     if len(user_request.split(" ")) > 1:
         check_search = user_request.split(" ")[0]
@@ -38,14 +38,38 @@ def get_bot_response():
                 response = scrape_data(user_request, "nearby")
             else:
                 response = scrape_data(user_request, "")
-                
         else:
-            response = chatbot_response(user_request)                
+            if user_request == 'how are you?' or user_request == 'how are you':
+                response= 'Fine , Good to see you again'
+            elif user_request == 'what is your name' or user_request == 'Who are you':
+                response = ' I am farmbot, I help in agriculture related problems'
+            else:
+                user_request = user_request.translate ({ord(c): "" for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+"})
+                check_query = user_request.split(" ")[1]
+                check_text = user_request.split(" ")[1:3]
+                if check_text == check_wikipedia1 or check_text == check_wikipedia2:
+                    response = scrape_data(user_request, "wikipedia")
+                elif check_text == check_wikihow:
+                    response = scrape_data(user_request, "wikihow")
+                elif check_query == "nearby":
+                    response = scrape_data(user_request, "nearby")
+                else:
+                    response = scrape_data(user_request, "")                
 
     else:
-        response = chatbot_response(user_request)
+        if user_request == 'hi':
+            response = 'Hello , Thanks for your greeting'
+        elif user_request == 'bye' or  user_request == 'thankyou':
+            response = 'bye, Thank you'
+        elif user_request == 'hello':
+            response = 'Good to see you again'
+        else:
+            response = 'hey'
+   
     
     return response
+
+
 
 if __name__ == "__main__":
     app.run(threaded=False)
